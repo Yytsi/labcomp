53fb03cc76f875c7002bfea6a6899e9bf0317d5d
Tuukka Yildirim <tuukka.yildirim@gmail.com>
2023-11-18 18:09:47 +0200

    lisää testejä

--- tests/binary_heap_test.cpp
+++ tests/binary_heap_test.cpp
@@ -67,3 +67,91 @@ TEST(BinaryHeapTest, ComplexOperations) {
 
     EXPECT_EQ(heap.getMinimum(), 5);
 }
+
+TEST(BinaryHeapTest, RepeatedInsertAndDelete) {
+    BinaryHeap heap;
+    for (int i = 0; i < 10; i++) {
+        heap.insert(i);
+        heap.deleteMinimum();
+    }
+
+    EXPECT_THROW(heap.getMinimum(), std::exception);
+}
+
+TEST(BinaryHeapTest, InsertDuplicateValues) {
+    BinaryHeap heap;
+    for (int i = 0; i < 5; i++) {
+        heap.insert(5);
+    }
+
+    for (int i = 0; i < 5; i++) {
+        EXPECT_EQ(heap.getMinimum(), 5);
+        heap.deleteMinimum();
+    }
+
+    EXPECT_THROW(heap.getMinimum(), std::exception);
+}
+
+TEST(BinaryHeapTest, InsertInDescendingOrder) {
+    BinaryHeap heap;
+    for (int i = 10; i > 0; i--) {
+        heap.insert(i);
+    }
+
+    for (int i = 1; i <= 10; i++) {
+        EXPECT_EQ(heap.getMinimum(), i);
+        heap.deleteMinimum();
+    }
+}
+
+TEST(BinaryHeapTest, RandomInsertAndDelete) {
+    BinaryHeap heap;
+    std::vector<int> numbers = {7, 3, 9, 1, 4, 8, 2, 5, 6, 0};
+
+    for (int num : numbers) {
+        heap.insert(num);
+    }
+
+    std::sort(numbers.begin(), numbers.end());
+    for (int expected : numbers) {
+        EXPECT_EQ(heap.getMinimum(), expected);
+        heap.deleteMinimum();
+    }
+}
+
+TEST(BinaryHeapTest, RandomTest) {
+    BinaryHeap heap;
+    for (int i = 0; i < 10000; i++) {
+        heap.insert(rand() % 10000);
+    }
+
+    int last = -1;
+    while (true) {
+        try {
+            int current = heap.getMinimum();
+            heap.deleteMinimum();
+            EXPECT_GE(current, last);
+            last = current;
+        } catch (std::exception&) {
+            break;
+        }
+    }
+}
+
+TEST(BinaryHeapTest, InsertNegativeNumbers) {
+    BinaryHeap heap;
+    heap.insert(-1);
+    heap.insert(-3);
+    heap.insert(-2);
+
+    EXPECT_EQ(heap.getMinimum(), -3);
+    heap.deleteMinimum();
+
+    EXPECT_EQ(heap.getMinimum(), -2);
+    heap.deleteMinimum();
+
+    EXPECT_EQ(heap.getMinimum(), -1);
+    heap.deleteMinimum();
+
+    EXPECT_THROW(heap.getMinimum(), std::exception);
+}
