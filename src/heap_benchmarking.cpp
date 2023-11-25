b95b0a82323e3dfce73b87b393f53461454e0b90
Tuukka Yildirim <tuukka.yildirim@gmail.com>
2023-11-25 21:36:33 +0200

    suorituskykytestaaja

--- src/heap_benchmarking.cpp
+++ src/heap_benchmarking.cpp
@@ -1,49 +1,59 @@
 #include "../src/binary_heap.cpp"
-#include <string>
+#include "../src/binomial_heap.cpp"
+#include "../src/d_ary_heap.cpp"
+#include "../src/pairing_heap.cpp"
+#include <vector>
 #include <iostream>
 #include <chrono>
+#include <typeinfo>
 
 using namespace std;
 using namespace std::chrono;
 
-void testWithInput(vector<pair<char, int>> actions) {
-  auto start = high_resolution_clock::now();
+template<typename HeapType>
+void testHeapWithInput(HeapType& heap, const vector<pair<char, int>>& actions) {
+    auto start = high_resolution_clock::now();
 
-  BinaryHeap bin;
-  for (pair<char, int> k : actions) {
-    char s = k.first;
-    int num = k.second;
-    if (s == 'a') {
-      bin.insert(num);
-    } else if (s == 'p') {
-      bin.deleteMinimum();
-    } else if (s == 'g') {
-      bin.getMinimum();
+    for (auto& k : actions) {
+        char s = k.first;
+        int num = k.second;
+        if (s == 'a') {
+            heap.insert(num);
+        } else if (s == 'p') {
+            heap.deleteMinimum();
+        } else if (s == 'g') {
+            heap.getMinimum();
+        }
     }
-  }
 
-  auto stop = high_resolution_clock::now();
-  auto duration = duration_cast<microseconds>(stop - start);
-  cout << "Testi vei aikaa: " << duration.count() << " mikrosekuntia" << endl;
-  cout << "Testi vei aikaa: " << duration.count()/1000000.0 << " sekuntia" << endl;
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << typeid(HeapType).name() << ": " << duration.count() << " mikrosekuntia" << endl;
+    cout << typeid(HeapType).name() << ": " << duration.count()/1000000.0 << " sekuntia" << endl;
 }
 
 int main() {
-  int n;
-  cin>>n;
+    int n;
+    cin >> n;
 
-  vector<pair<char, int>> actions;
+    vector<pair<char, int>> actions;
+    for (int i = 0; i < n; i++) {
+        char s;
+        cin >> s;
+        int x = -1;
+        if (s == 'a') {
+            cin >> x;
+        }
+        actions.push_back({s, x});
+    }
 
-  for (int i = 0; i < n; i++) {
-    char s;
-    cin>>s;
-    
-    if (s == 'a') {
-      int x;
-      cin>>x;
-      actions.push_back({s, x});
-    } else actions.push_back({s, -1});
-  }
+    BinaryHeap binaryHeap;
+    BinomialHeap binomialHeap;
+    DaryHeap daryHeap(4); // Create a D-ary heap with D = 4
+    PairingHeap pairingHeap;
 
-  testWithInput(actions);
+    testHeapWithInput(binaryHeap, actions);
+    testHeapWithInput(binomialHeap, actions);
+    testHeapWithInput(daryHeap, actions);
+    testHeapWithInput(pairingHeap, actions);
 }
