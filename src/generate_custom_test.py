e7938c22db90f653c0fa3af19b591224b8c88d27
Tuukka Yildirim <tuukka.yildirim@gmail.com>
2023-12-02 18:15:13 +0200

    laadukkaampaa testienluontia

--- src/generate_custom_test.py
+++ src/generate_custom_test.py
@@ -1,7 +1,7 @@
 import random
 import os
 
-def generate_test_file(filename, num_operations, max_value):
+def generate_random_test_file(filename, num_operations, max_value):
     with open(filename, 'w') as file:
         heap_size = 0
         operations = []
@@ -25,8 +25,55 @@ def generate_test_file(filename, num_operations, max_value):
         file.write(f'{len(operations)}\n')
         file.write('\n'.join(operations))
 
-filename = input("Give a name for the test file (such as big_text.txt): ")
-num_operations = int(input("Enter the number of operations for the test file (insert, get min, delete min): "))  # Number of operations in the test (insert, get min, delete min)
-max_value = int(input("Enter the max value that is inserted to the heap: ")) # Max value that is inserted to the heap
+def generate_add_only_test_file(filename, num_operations, max_value):
+    with open(filename, 'w') as file:
+        operations = ['a ' + str(random.randint(1, max_value+1)) for _ in range(num_operations)]
+        file.write(f'{len(operations)}\n')
+        file.write('\n'.join(operations))
 
-generate_test_file(filename, num_operations, max_value)
+def generate_add_and_get_test_file(filename, initial_adds, num_operations, max_value):
+    with open(filename, 'w') as file:
+        operations = ['a ' + str(random.randint(1, max_value+1)) for _ in range(initial_adds)]
+        operations += ['g' for _ in range(num_operations)]
+        file.write(f'{len(operations)}\n')
+        file.write('\n'.join(operations))
+
+def generate_add_and_remove_test_file(filename, num_operations, max_value):
+    with open(filename, 'w') as file:
+        heap_size = 0
+        operations = []
+        for _ in range(num_operations):
+            if heap_size == 0 or random.choice([True, False]):
+                value = random.randint(1, max_value+1)
+                operations.append(f'a {value}')
+                heap_size += 1
+            else:
+                operations.append('p')
+                heap_size -= 1
+
+        file.write(f'{len(operations)}\n')
+        file.write('\n'.join(operations))
+
+
+print("Valitse testitiedoston tyyppi:")
+print("1. Satunnaiset operaatiot (lisää, poista, hae)")
+print("2. Vain lisäysoperaatiot")
+print("3. Lisäys- ja hakuoperaatiot")
+print("4. Lisäys- ja poisto-operaatiot")
+
+test_file_type = int(input("Anna testitiedoston tyyppi (1-4): "))
+filename = input("Anna testitiedoston nimi (esim. big_test.txt): ")
+num_operations = int(input("Anna operaatioiden määrä testitiedostossa: "))
+max_value = int(input("Anna suurin arvo, joka voidaan lisätä kekoon: "))
+
+if test_file_type == 1:
+    generate_random_test_file(filename, num_operations, max_value)
+elif test_file_type == 2:
+    generate_add_only_test_file(filename, num_operations, max_value)
+elif test_file_type == 3:
+    initial_adds = int(input("Anna alustavien lisäysoperaatioiden määrä: "))
+    generate_add_and_get_test_file(filename, initial_adds, num_operations, max_value)
+elif test_file_type == 4:
+    generate_add_and_remove_test_file(filename, num_operations, max_value)
+else:
+    print("Virheellinen syöte. Anna numero väliltä 1-4.")
